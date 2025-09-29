d8f488caeb856b066e2684b0885c2639db416c1a
marcello <williammarcello30@gmail.com>
2025-09-29 11:53:10 +0700

    navbar

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// navabr
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
+
+function closeMenu() {
+    document.getElementById("navLinks").classList.remove("active");
+  }
+// akhir navbar
