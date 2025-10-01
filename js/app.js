82a4c9d55a652b8f313b061ba42cb15375bffae2
marcello <williammarcello30@gmail.com>
2025-10-01 15:57:52 +0700

    update flash sale product OnMarket

--- js/app.js
+++ js/app.js
@@ -66,3 +66,31 @@ addToCartButtons.forEach(button => {
 });
 
 // akhir fitur keranjang
+
+// Flash sale countdown
+function startCountdown(duration, display) {
+  let timer = duration, hours, minutes, seconds;
+  setInterval(function () {
+    hours = parseInt(timer / 3600, 10);
+    minutes = parseInt((timer % 3600) / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = hours + ":" + minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      display.textContent = "Selesai";
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  let countdownTime = 5 * 60 * 60;
+  let display = document.querySelector('#timer');
+  startCountdown(countdownTime, display);
+};
+
+// akhir flash sale countdown
